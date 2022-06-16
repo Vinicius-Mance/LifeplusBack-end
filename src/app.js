861e62b378fe803c1fd53f5c21bafa649f8038db
josersn <nettorammos@hotmail.com>
2022-06-16 12:41:54 -0300

    new end-point

--- src/app.js
+++ src/app.js
@@ -74,6 +74,52 @@ app.get("/historic", (req, res) => {
     return res.status(200).json({
         data: historic
     })
+});
+
+
+app.get("/getGraph", (req, res) => {
+    data = [
+        {
+            date: "14/06/2022",
+            data: [
+                { name: 'Page A', pv: 2400, amt: 2400 },
+                { name: 'Page B', pv: 1398, amt: 2210 },
+                { name: 'Page C', pv: 9800, amt: 2290 },
+                { name: 'Page D', pv: 3908, amt: 2000 },
+                { name: 'Page E', pv: 4800, amt: 2181 },
+                { name: 'Page F', pv: 3800, amt: 2500 },
+                { name: 'Page G', pv: 4300, amt: 2100 }
+            ]
+        },
+        {
+            date: "15/06/2022",
+            data: [
+                { name: 'Page A', pv: 2400, amt: 2400 },
+                { name: 'Page B', pv: 1398, amt: 2210 },
+                { name: 'Page C', pv: 9800, amt: 2290 },
+                { name: 'Page D', pv: 3908, amt: 2000 },
+                { name: 'Page E', pv: 4800, amt: 2181 },
+                { name: 'Page F', pv: 3800, amt: 2500 },
+                { name: 'Page G', pv: 4300, amt: 2100 }
+            ]
+        },
+        {
+            date: "16/06/2022",
+            data: [
+                { name: 'Page A', pv: 2400, amt: 2400 },
+                { name: 'Page B', pv: 1398, amt: 2210 },
+                { name: 'Page C', pv: 9800, amt: 2290 },
+                { name: 'Page D', pv: 3908, amt: 2000 },
+                { name: 'Page E', pv: 4800, amt: 2181 },
+                { name: 'Page F', pv: 3800, amt: 2500 },
+                { name: 'Page G', pv: 4300, amt: 2100 }
+            ]
+        }
+    ];
+
+    return res.status(200).json({
+        data: data
+    })
 })
 
 app.listen(8080, () => console.log("Serve is running 🐱‍🏍"))
